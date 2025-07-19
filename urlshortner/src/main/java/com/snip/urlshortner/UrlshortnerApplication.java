542cf80e72c40418b193d4a7f7f303c01f446b9a
Utkarsh Shankar <utkarsh.shankar100@gmail.com>
2025-07-19 20:49:26 +0530

    Add files via upload

---
+++ urlshortner/src/main/java/com/snip/urlshortner/UrlshortnerApplication.java
@@ -0,0 +1,15 @@
+package com.snip.urlshortner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlshortnerApplication {
+
+	public static void main(String[] args) {
+		System.out.println("HWllo");
+
+		SpringApplication.run(UrlshortnerApplication.class, args);
+	}
+
+}
